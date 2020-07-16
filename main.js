1e958d069414453a6ab36cf9fa1c215bec432172
Andrew Garnett <andrewgarnett@Andrews-Mac-mini.local>
2020-07-16 12:32:38 -0400

    conditionals'

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
-var name = "Andrew"
-var int = 3
-console.log("Andrew" * 3)
+var name = "Andrew";
+var int = 3;
+console.log("Andrew" * 3);
+
+
+var name = "True";
+var int = 2;
+console.log("True" + 2);
+
+var widget = 2;
+widget = widget + 5;
+console.log(widget);
+console.log(widget * 2);
+console.log(14 % 3);
+
+var sprocket = 10;
+if (sprocket = 10){
+  console.log("sprocket is an even number");
+}else{
+  console.log("sprocket is an odd number");
+}
+
+
+var areYouSmart = false;
+var takeMoreClasses = false;
+
+if(areYouSmart){
+  console.log("smart!");
+} else {
+  if(takeMoreClasses){
+    console.log("getting  there");
+  } else {
+    console.log("smart!");
+  }
+}
+
 
 
