cae9bb09bc1284651edf47ac4087124de68d770e
Andrew Garnett <andrewgarnett@Andrews-Mac-mini.local>
2020-07-19 17:40:47 -0400

    funjs

--- fun.js
+++ fun.js
@@ -1,1 +1,51 @@
-fun.js
+function findArea(width, height) {
+	var dimension = width * height;
+	return dimension;
+  }  
+ 
+ console.log("Your dimension is: " +  findArea(10, 15));
+
+
+ /*Adding function with two args*/
+function findArea(width, height) {
+	var dimension = width + height; {
+	console.log("the dimension is: " + findArea(10, 15)); 
+ }  
+}
+
+findArea()
+  
+
+  
+  /*function for looping through numbers*/
+
+function numCount() {
+	for (first = 0; first < 100; first++) {
+	  console.log(first);
+	}
+  
+  }
+  
+  numCount()
+
+  /*scopes*/
+  function myDog() {
+	var name = "Brownie";
+	return name;
+  }
+	   
+  
+  var myMeanDog = myDog();
+  console.log(myMeanDog);
+  console.log(name); 
+  
+  myDog()
+
+  /*IIFE*/
+  (function numCount() {
+	for (first = 0; first < 100; first++) {
+	  console.log(first);
+	}
+  
+  })()
+  
