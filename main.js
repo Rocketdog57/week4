a8dcd5c56657855d6fa28ba9cac181c1656218f0
Andrew Garnett <andrewgarnett@Andrews-Mac-mini.local>
2020-07-19 15:01:49 -0400

    wk2 hw2

--- main.js
+++ main.js
@@ -34,5 +34,16 @@ if(areYouSmart){
   }
 }
 
+var count = 2;
+count = (count + 5) * 2;
+console.log(count);
+console.log(count%3);
 
 
+
+var num = 10;
+   if(num % 2 == 0) {
+      console.log("Number is even!");
+   } else {
+      console.log("Number is odd!");
+   }
